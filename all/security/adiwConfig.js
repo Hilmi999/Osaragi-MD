4b8b0adc618c94f89d4fae4d75370a1e2dafc35e
Hilmi999 <boengahkupi@gmail.com>
2024-12-16 10:17:00 +0800

    Update adiwConfig.js

--- all/security/adiwConfig.js
+++ all/security/adiwConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
   'approval': {
-    'num': '6289508082845',
+    'num': '6287878244661',
     'text': "oke",
     'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
     'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
   },
-  'creatorScript': '6289508082845',
+  'creatorScript': '6287878244661',
   'filePath': "./all/approval",
   'checkFilePath': "../setting/osaragi.js",
   'codeToDetect': 'main();'
